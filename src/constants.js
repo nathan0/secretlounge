0a770aa99c975b349a9f2006867dc401021aafe6
Daniel Bugl <me@omnidan.net>
2016-10-22 20:07:50 +0200

    tweak spam scoring constants

--- src/constants.js
+++ src/constants.js
@@ -6,9 +6,9 @@ export const SPAM_LIMIT = 3 // score / messages
 export const SPAM_LIMIT_HIT = 5 // set score to this when limit is reached
 export const SPAM_INTERVAL = 5 * SECONDS // interval to decrease scores
 
-export const SCORE_MESSAGE = 1 // score per message sent
-export const SCORE_LINK = 0.5  // score per message link
-export const SCORE_CHARACTER = 0.01 // score per message character
+export const SCORE_MESSAGE = 0.75 // score per message sent
+export const SCORE_LINK = 0.25  // score per message link
+export const SCORE_CHARACTER = 0.004 // score per message character
 export const SCORE_STICKER = 1.5 // score per sticker
 
 export const BASE_COOLDOWN_MINUTES = 5
